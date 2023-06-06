d890b9a339485c9e1a415714361766b0003047f1
tiagospp55 <83403625+tiagospp55@users.noreply.github.com>
2023-06-06 14:54:12 +0100

    UART callback

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@ K_THREAD_STACK_DEFINE(buttonStack, STACK_SIZE);
 struct k_thread buttonData;
 k_tid_t ledID;
 void buttonThread(void *argA, void *argB, void *argC);
-
+volatile uint8_t buttonPressed;
 
 #define I2CPRIORITY 1
 volatile uint16_t ic2Period = 1000;
@@ -76,12 +76,14 @@ const struct uart_config uartConfig = {
 
 void uartThread(void *argA, void *argB, void *argC);
 
-const struct device *uart_dev;
+const struct device *uartDev;
 uint8_t rxBuffer[RXBUFFERSIZE];
-uint8_t txBuffer[RXBUFFERSIZE];
+uint8_t rxData[RXBUFFERSIZE];
+volatile uint16_t uartReceiverUsed;
 
 void uartCallback(const struct devide *dev, struct uartEvent *event, void *data);
 
+struct k_sem semUart;
 void main(void){
 
 }
@@ -158,5 +160,85 @@ void i2cThread(void *argA, void *argB, void *argC){
     timing_stop();
 }
 
+void uartThread(void *argA, void *argB, void *argC){
+    int ret = 0;
+    char data;
+
+    uint8_t transmitMessage[TXBUFFERSIZE];
+
+     while(1){
+        k_sem_take(&semUart, K_FOREVER);
+
+        if(uartReceiverUsed > 0){
+            rxData[uartReceiverUsed] = 0;
+            ret = uart_tx(uartDev, transmitMessage, strlen(transmitMessage), SYS_FOREVER_MS));
+
+            if(ret){
+                return;
+            }
+
+            data = rxData[uartReceiverUsed - 1];
+
+            if(data == 0xd){
+                for(int i = 0; i <= uartReceiverUsed; i++){
+                    command[k] = rxData[k];
+                    rxData[k] = 0;
+                }
+                sizeOfCommand = uartReceiverUsed;
+
+                cmdProcessor(); 
+            }
+        }
+        k_msleep(1);
+     }
+}
 
 
+
+void buttonThread(void * argA, void *argB, void *argC){
+    uint64_t finTime = 0;
+    uint64_t releaseTime = 0;
+
+    releaseTime = k_uptime_get() + buttonThread;
+
+    while(1){
+
+        for(int i = 0; i < 4; i++){
+            if(buttonPressed == i){
+                RtData.buttonState[i] = !RtData.buttonState[i] 
+            }
+        }
+
+        buttonPressed = 0;
+
+        finTime = k_uptime_get();
+
+        if(finTime < releaseTime){
+            k_msleep(releaseTime - finTime);
+            releaseTime = releaseTime + ic2Period;
+        }
+    } 
+
+    timing_stop();
+}
+
+
+void uartCallback(const struct devide *dev, struct uartEvent *event, void *data){
+    int ret = 0; 
+
+    switch(event->type)
+    {
+    case UART_TX_DONE:
+        break;
+    case UART_RX_RDY:
+        memcpy(&rxData[uartReceiverUsed], &rxBuffer[event->data.rx.offset], event->data.rx.len);
+        uartReceiverUsed++;
+        k_sem_give(&semUart);
+    case UART_TX_ABORTED:
+        break;
+    case UART_RX_DISABLED:
+             err =  uart_rx_enable(uartDev ,rxBuffer,sizeof(rxBuffer),RX_TIMEOUT);
+    default:
+        break;
+    }
+}
